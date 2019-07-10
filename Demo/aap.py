37bc106fc6173bdcd220abc3ebefbccbd75e2205
zytwo <250132170@qq.com>
2019-07-10 14:59:54 +0800

    change name

--- Demo/aap.py
+++ Demo/aap.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/hello/",methods=['POST','GET'])
 def hello():
-    return 'hello world'
+    return 'hello world zy'
 
 if __name__ == "__main__":
     app.run()
