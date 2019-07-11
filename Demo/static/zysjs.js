9f6d773d2cf6830d8fcf287ab3fd6ee02342ce3a
zy <155182952@qq.com>
2019-07-11 17:54:19 +0800

    2019-7-11

--- Demo/static/zysjs.js
+++ Demo/static/zysjs.js
@@ -1,16 +1,34 @@
 new Vue({
+    delimiters: ["[[", "]]"],
     el: '#app',
     data: {
-        use: true,
-        id: 2,
-        url: 'http://www.baidu.com',
-        message: 'it is not my fault'
+        iphones: [{
+            id: 1,
+            name: 'iphone 8',
+            price: 5099,
+            count: 1
+        },
+            {
+                id: 2,
+                name: 'iphone xs',
+                price: 8699,
+                count: 1
+            },
+            {
+                id: 3,
+                name: 'iphone xr',
+                price: 6499,
+                count: 1
+            }]
     },
-    methods: {
-        dome: function (e) {
-            alert('aa');
-            console.log(e)
-
+    watch: {
+        ten: function (val) {
+            this.ten = val;
+            this.one = val * 10
+        },
+        one: function (val) {
+            this.ten = val / 10;
+            this.one = val
         }
     }
 })
