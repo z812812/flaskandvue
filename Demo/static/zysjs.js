1a82be9a7ce14923f838a48fe0865606a6d1e2c3
zy <155182952@qq.com>
2019-07-12 14:25:07 +0800

    test vue files

--- Demo/static/zysjs.js
+++ Demo/static/zysjs.js
@@ -1,34 +1,16 @@
 new Vue({
     delimiters: ["[[", "]]"],
     el: '#app',
-    data: {
-        iphones: [{
-            id: 1,
-            name: 'iphone 8',
-            price: 5099,
-            count: 1
-        },
-            {
-                id: 2,
-                name: 'iphone xs',
-                price: 8699,
-                count: 1
-            },
-            {
-                id: 3,
-                name: 'iphone xr',
-                price: 6499,
-                count: 1
-            }]
+    data:{
+        mys:'sb',
+        da:[
+            'gg','ll','kk','pp'
+        ]
     },
-    watch: {
-        ten: function (val) {
-            this.ten = val;
-            this.one = val * 10
-        },
-        one: function (val) {
-            this.ten = val / 10;
-            this.one = val
+    methods:{
+        change:function () {
+            alert("你是sb吗")
         }
     }
+
 })
